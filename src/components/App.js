4c1226e7b834c13d176019fbe658d876ee53351f
nevraka <nevrakaya@gmail.com>
2020-09-05 23:11:42 +0200

    created App.js

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Login from './Login';
+import { getTokenFromUrl } from '../api/spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
+
+const App = () => {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  //Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+    }
+  }, []);
+
+  return <div>{token ? <Player spotify={spotify} /> : <Login />}</div>;
+};
+
+export default App;
