f0b7e14804ae509260beda79ba622089d400cf9d
nevraka <nevrakaya@gmail.com>
2020-09-18 16:02:05 +0200

    created index css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { DataLayer } from './components/DataLayer';
 import reducer, { initialState } from './components/reducer';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
