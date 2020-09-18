7d3cdc63394289e0bb0a57d46aa2c0f6d7101c3f
nevraka <nevrakaya@gmail.com>
2020-09-18 16:03:46 +0200

    added sidebar and body

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './Player.css';
 import { useDataLayerValue } from './DataLayer';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 
 const Player = ({ spotify }) => {
   const [_, dispatch] = useDataLayerValue();
 
   return (
     <div className="player">
-      <div className="player__body"></div>
-      {/*sidebar*/}
-      {/*header*/}
-      <button
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      {/* <button
         onClick={() => {
           dispatch({ type: 'SET_TOKEN', token: null });
         }}
       >
         Log Out
-      </button>
-      {/*footer*/}
+      </button> */}
+      <Footer />
     </div>
   );
 };
